99c6987407e997683aa7d0da25f413976eef4d16
Yubin Cho <yucho@ucsd.edu>
2017-01-26 08:42:16 -0800

    added change

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,27 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
+	$("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e){
+	e.preventDefault();
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+    //if (description.length == 0) {
+    	$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    	$("p").fadeOut("slow");
+    //}
+   /* else {
+    	description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }*/
+
 }
